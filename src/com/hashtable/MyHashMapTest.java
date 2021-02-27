b895080e5a1ceda37817e7e5a96b3ecdad1039ee
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-28 00:34:54 +0530

    Ability to find frequency of words in large paragraph

--- src/com/hashtable/MyHashMapTest.java
+++ src/com/hashtable/MyHashMapTest.java
@@ -4,6 +4,7 @@ package com.hashtable;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import com.hashtable.MyHashMap;
+import com.hashtable.MyLinkedHashMap;
 
 public class MyHashMapTest {
 
@@ -22,6 +23,24 @@ public class MyHashMapTest {
 		}
 		int frequency = myHashMap.get("to");
 		assertEquals(2, frequency);
+	}
+
+	@Test
+	public void givenASentenceWhenWordsAreAddedShouldReturnWordFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but "
+				+ "because they keep putting themselves deliberately into " + "paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value = value + 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
+		assertEquals(3, frequency);
 	}
 }
 
---
+++ src/com/hashtable/MyLinkedHashMap.java
@@ -0,0 +1,53 @@
+package com.hashtable;
+
+import java.util.ArrayList;
+
+public class MyLinkedHashMap<K, V> {
+
+	private final int numBuckets;
+	ArrayList<MyLinkedList<K>> myBucketArray;
+
+	public MyLinkedHashMap() {
+		this.numBuckets = 10;
+		this.myBucketArray = new ArrayList<>(numBuckets);
+		for (int i = 0; i < numBuckets; i++) { // create empty LinkedList
+			this.myBucketArray.add(null);
+		}
+	}
+
+	private int getBucketIndex(K key) {
+		int hashCode = Math.abs(key.hashCode());
+		int index = hashCode % numBuckets;
+		return index;
+	}
+
+	public V get(K key) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if (myLinkedList == null)
+			return null;
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.searchNode(key);
+		return (myMapNode == null) ? null : myMapNode.getValue();
+	}
+
+	public void add(K key, V value) {
+		int index = this.getBucketIndex(key);
+		MyLinkedList<K> myLinkedList = this.myBucketArray.get(index);
+		if (myLinkedList == null) {
+			myLinkedList = new MyLinkedList<>();
+			this.myBucketArray.set(index, myLinkedList);
+		}
+		MyMapNode<K, V> myMapNode = (MyMapNode<K, V>) myLinkedList.searchNode(key);
+		if (myMapNode == null) {
+			myMapNode = new MyMapNode<>(key, value);
+			myLinkedList.appendAfter(myMapNode);
+		} else {
+			myMapNode.setValue(value);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "MyLinkedHashMap List{" + myBucketArray + '}';
+	}
+}
